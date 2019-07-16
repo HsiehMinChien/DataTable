fbd0ac2c5dc67e80aefb6c4de8ef88d3b5027d3b
MC <mc@MCde-MacBook-Air.local>
2019-07-16 13:24:35 +0800

    modify

--- components/pages/omdbDemo/index.tsx
+++ components/pages/omdbDemo/index.tsx
@@ -1,15 +1,51 @@
 import React from 'react';
 import _ from 'lodash';
+import styled from 'styled-components';
 import Container from '@material-ui/core/Container';
-import { RippleSpinner, convertToOmdbApiURL } from '../..';
+import { Button, Row, Col } from 'reactstrap';
+import { RippleSpinner, convertToOmdbApiURL, CustomzieInput } from '../..';
+
+enum InputType {
+  title = 'title',
+  year = 'year',
+  type = 'type'
+}
+
+type InputTypeList = keyof typeof InputType
+
+const StyledImg = styled.img`
+  width: 150px;
+  height: 222.5px;
+  background-color: #eee;
+  object-fit: cover;
+  border: 0px;
+`;
+
+const StyledFlex = styled.div`
+  display: flex | inline-flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  margin-right: 20px;
+  width: 160px;
+`;
+
+const StyledRow = styled(Row)`
+  width: 150px;
+`
 
 export const OmdbDemo = () => {
+  const titleRef = React.useRef(null);
+  const yearRef = React.useRef(null);
+
+  const [title, setTitle] = React.useState('');
+  const [year, setYear] = React.useState('');
+
   const [data, setData] = React.useState<any>(null);
   const [isTrigger, setIsTrigger] = React.useState(false)
   const [isLoaded, setIsLoaded] = React.useState(true);
 
   const handleClick = () => {
-    fetch(convertToOmdbApiURL('iron man', '2008', 'movie'))
+    fetch(convertToOmdbApiURL(title, year, 'movie'))
       .then(res => res.json())
       .then(
         (result) => {
@@ -26,6 +62,19 @@ export const OmdbDemo = () => {
       });
   }
 
+  const handleOnChange = (e: any, t: InputTypeList) => {
+    switch (t) {
+      case InputType.title:
+        setTitle(e.target.value);
+        break;
+      case InputType.year:
+        setYear(e.target.value);
+        break;
+      default:
+        break;
+    }
+  }
+
   const convertDataToDisplay = () => {
     if (
       !_.isNil(data) &&
@@ -36,12 +85,15 @@ export const OmdbDemo = () => {
       !_.isNil(data) &&
       _.get(data, 'Response', null) === 'True' &&
       _.isNil(_.get(data, 'Error', null)) &&
-      _.isArray(_.get(data, 'Search', []))
+      _.isArray(_.get(data, 'Search', null))
     ) {
       return data.Search.map((item: any, i: number) => (
-        <div key={`${item}${i}`}>
-          <div>Title: {item.Title}</div>
-        </div>
+        <StyledFlex key={`${item}${i}`}>
+          <div>
+            <StyledRow><StyledImg src={item.Poster} /></StyledRow>
+            <StyledRow>{item.Title}</StyledRow>
+          </div>
+        </StyledFlex>
       ))
     }
     console.log(data);
@@ -50,17 +102,41 @@ export const OmdbDemo = () => {
 
   const renderContent = () => {
     if (!isTrigger && isLoaded) {
-      return <button
-        onClick={() => {
-          setIsLoaded(false);
-          setIsTrigger(true);
-          handleClick();
-        }}>Fetch</button>;
+      return <>
+        <CustomzieInput
+          isValid={false}
+          title={'Title'}
+          placeholder={''}
+          refs={titleRef}
+          onBlur={() => { }}
+          onChange={e => handleOnChange(e, InputType.title)}
+          warningString={''}
+          value={title}
+        />
+        <CustomzieInput
+          isValid={false}
+          title={'Year'}
+          placeholder={''}
+          refs={yearRef}
+          onBlur={() => { }}
+          onChange={e => handleOnChange(e, InputType.year)}
+          warningString={''}
+          value={year}
+        />
+        <br />
+        <Button
+          color="primary"
+          onClick={() => {
+            setIsLoaded(false);
+            setIsTrigger(true);
+            handleClick();
+          }}>Fetch</Button>
+      </>;
     } else if (isTrigger && isLoaded) {
       return <div>
         {convertDataToDisplay()}
-        Done<br />
-        <button onClick={() => setIsTrigger(false)}>Back to Search</button>
+        <br />
+        <Button onClick={() => setIsTrigger(false)}>Back to Search</Button>
       </div>;
     } else {
       return <RippleSpinner />;
