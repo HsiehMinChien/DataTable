0de0d6aed3d3026fed8117d79860e41278aecef7
MC <mc@MCde-MacBook-Air.local>
2020-01-24 22:17:48 +0800

    Add now deploy

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -20,6 +20,13 @@ export default class MyDocument extends Document {
     return (
       <html lang="en">
         <Head>
+          <title>Offline Next.js with Now 2.0</title>
+          <link rel="manifest" href="/manifest.json" />
+          <meta name="theme-color" content="#72B340" />
+          <meta
+            name="description"
+            content="make your Next.js application work offline using service workers via Google's workbox"
+          />
           <meta name="viewport" content="initial-scale=1.0, width=device-width" />
           <link rel="stylesheet" type="text/css" href="static/fonts/css/all.css" />
         </Head>
