e3be77285406a3e18dae1ed645eaf42c2790e7a3
MC <mc@MCde-MacBook-Air.local>
2020-01-27 21:18:06 +0800

    Update

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,34 +1,39 @@
 import React from 'react';
-import Document, { Head, Main, NextScript } from 'next/document';
+import Document, { Head, Main, NextScript, DocumentContext, } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
-  static async getInitialProps(ctx: any) {
-    const sheet = new ServerStyleSheet();
-    const originalRenderPage = ctx.renderPage;
-    ctx.renderPage = () =>
-      originalRenderPage({
-        enhanceApp: (App: any) => (props: any) => sheet.collectStyles(<App {...props} />),
-        ehhanceComponent: (Component: any) => (props: any) =>
-          sheet.collectStyles(<Component {...props} />)
-      });
-    const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps, styles: [initialProps.styles, ...sheet.getStyleElement()] };
+  static async getInitialProps(ctx: DocumentContext) {
+    const sheet = new ServerStyleSheet()
+    const originalRenderPage = ctx.renderPage
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App: any) => (props: any) => sheet.collectStyles(<App {...props} />),
+        })
+
+      const initialProps = await Document.getInitialProps(ctx)
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      }
+    } finally {
+      sheet.seal()
+    }
   }
 
   render() {
     return (
       <html lang="en">
         <Head>
-          <title>Offline Next.js with Now 2.0</title>
-          <link rel="manifest" href="/manifest.json" />
-          <meta name="theme-color" content="#72B340" />
-          <meta
-            name="description"
-            content="make your Next.js application work offline using service workers via Google's workbox"
-          />
           <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-          <link rel="stylesheet" type="text/css" href="static/fonts/css/all.css" />
+          <link rel="stylesheet" type="text/css" href="fonts/css/all.css" />
         </Head>
         <body>
           <Main />
